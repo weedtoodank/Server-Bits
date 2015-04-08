35ea4b25c857d8354ccdbfc0e83d5fc72a6a8627
weedtoodank <jesuscgaeboyz@gmail.com>
2015-04-07 17:54:54 -0700

    Update workshop.lua

--- workshop.lua
+++ workshop.lua
@@ -1,23 +1,35 @@
-resource.AddWorkshop("384708246")
-resource.AddWorkshop("376695298")
-resource.AddWorkshop("372767154")
-resource.AddWorkshop("372531919")
-resource.AddWorkshop("314000749")
-resource.AddWorkshop("295788137")
-resource.AddWorkshop("286968546")
-resource.AddWorkshop("284128010")
-resource.AddWorkshop("283211438")
-resource.AddWorkshop("261062245")
-resource.AddWorkshop("216408135")
-resource.AddWorkshop("121961644")
-resource.AddWorkshop("201027715")
-resource.AddWorkshop("201027186")
-resource.AddWorkshop("183140076")
-resource.AddWorkshop("183139624")
-resource.AddWorkshop("181656972")
-resource.AddWorkshop("181283903")
-resource.AddWorkshop("180507408")
-resource.AddWorkshop("113495466")
-resource.AddWorkshop("112806637")
-resource.AddWorkshop("108922813")
-resource.AddWorkshop("105115318")
+resource.AddWorkshop("384708246")---Phys / Gravity Gun [model/reskin]---
+resource.AddWorkshop("376695298")---HD FX 2---
+resource.AddWorkshop("372767154")---Green Arrow(CW) Playermodel---
+resource.AddWorkshop("372531919")---Christian Bale Playermodel---
+resource.AddWorkshop("314000749")---COD BO2 - CDC Soldier(Ragdoll+PlayerModel+NPC)---
+resource.AddWorkshop("295788137")---WATCH_DOGS:Aiden Pearce Playermodel
+resource.AddWorkshop("286968546")---Baseball Bat---
+resource.AddWorkshop("284128010")---Steampunk Plague Docotor---
+resource.AddWorkshop("283211438")---Don Vito Corleone---
+resource.AddWorkshop("216408135")---GTA 5 Playermodels---
+resource.AddWorkshop("121961644")---NYPD 1940's Model-Group
+resource.AddWorkshop("201027715")--- FA:S 2.0 Alpha SWEPs - U.Rifles---
+resource.AddWorkshop("201027186")---FA:S 2.0 Alpha SWEPs - Misc---
+resource.AddWorkshop("183140076")---FAS 2.0 Alpha SWEPs - Shotguns---
+resource.AddWorkshop("183139624")---FAS 2.0 Alpha SWEPs - SMGs---
+resource.AddWorkshop("181656972")---FA:S 2.0 Alpha SWEPs - Rifles---
+resource.AddWorkshop("181283903")---FA:S 2.0 Alpha SWEPS - Pistols---
+resource.AddWorkshop("180507408")---FA:S 2.0 Alpha SWEPs---
+resource.AddWorkshop("113495466")---Climb SWEP 2---
+resource.AddWorkshop("112806637")---GMod Legs---
+resource.AddWorkshop("108922813")---Realistic Blood---
+resource.AddWorkshop("105115318")---Fading Doors---
+resource.AddWorkshop("245700126")---Splinter Cell Spy and Merc
+resource.AddWorkshop("109643223")---3D2D Textscreens---
+resource.AddWorkshop("293535327")---Push Mod (Player Version)---
+resource.AddWorkshop("184512071")---Metal Gear Solid3: Big Boss Playermodel---
+resource.AddWorkshop("263446966")---Cry of Fear Simon Playermodel (half-dead)---
+resource.AddWorkshop("412672563")---Suits and Robbers Player Models [REUPLOADED]---
+resource.AddWorkshop("268401665")---BraxATM---
+resource.AddWorkshop("389445061")---DarkRP Bank Robbery System---
+resource.AddWorkshop("251645873")---Batclaw SWEP---
+resource.AddWorkshop("370857063")---Morgan Freeman Playermodel---
+resource.AddWorkshop("394381358")---Injustice Gods Among Us Batman Player/Npc---
+resource.AddWorkshop("328494560")---Nightwing NPC/Playermodel---
+resource.AddWorkshop("220336312")---PermaProps---
